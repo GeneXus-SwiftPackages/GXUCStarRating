de3188fb5962650a96cbd0c35a0bd4811964ab21
genexusbot <builder@genexus.com>
2023-03-27 17:10:24 -0300

    Package version: 1.0.0-beta+20230327200102

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCStarRating",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCStarRating-1.0.0-beta+20230324153906.xcframework.zip",
-			checksum: "09b253fa45df963fb4d5a183b8da32b6483175d0b2b267158d6de00a35d6d5af"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCStarRating-1.0.0-beta+20230327200102.xcframework.zip",
+			checksum: "7c580ad03363897a5eec1b96159d178bd94e7e9e7ddbf276bd8a9df9ebaf798c"
 		)
 	]
 )
