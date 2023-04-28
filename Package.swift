28f28d4e3adf050d1bf13c6feec8d23d328f2309
Build Er <builder@macmini6.local>
2023-04-28 17:33:32 -0300

    Package version: 1.0.0-beta+20230428202437

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCStarRating",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCStarRating-1.0.0-beta+20230428153652.xcframework.zip",
-			checksum: "311754950add40992a9a00281a36aaa3653ac01f58f82150da20a01e80ba85f9"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCStarRating-1.0.0-beta+20230428202437.xcframework.zip",
+			checksum: "ae1d2819ceee2392aac2bdb69b979ede839d0fb45af8780326b96a1557ead3ec"
 		)
 	]
 )
