e57ddf7519679ccb55e971926966717355cefd2c
Build Er <builder@macmini6.local>
2023-05-09 08:39:24 -0300

    Package version: 1.0.0-beta+20230509112137

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
 		.target(name: "GXUCStarRatingWrapper",
 				dependencies: [
 					"GXUCStarRating",
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .tvOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCStarRating",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCStarRating-1.0.0-beta+20230505145632.xcframework.zip",
-			checksum: "a48ed5e75a1b2732a7fe574272cfd4054ee546d87d83a30db3ddc36c8e124669"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCStarRating-1.0.0-beta+20230509112137.xcframework.zip",
+			checksum: "916e14153bb2dbd01ff8f683e93d50430e75be288f13f4621d17a30002eb9d60"
 		)
 	]
 )
