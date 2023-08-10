3fcfae0afa21f7c1b3d872db1f9665b8154d0faf
genexusbot <builder@genexus.com>
2023-08-10 11:11:30 -0300

    Package version: 1.1.0-beta.10

--- Package.swift
+++ Package.swift
@@ -10,19 +10,19 @@ let package = Package(
 			targets: ["GXUCStarRatingWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0-beta.9")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0-beta.10")
 	],
 	targets: [
 		.target(name: "GXUCStarRatingWrapper",
 				dependencies: [
 					"GXUCStarRating",
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .iOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .tvOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCStarRating",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCStarRating-1.1.0-beta.9.xcframework.zip",
-			checksum: "1d1f8484073992048f0856ceb619b74ca61f40d3b3a404b65127d436072cd69a"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCStarRating-1.1.0-beta.10.xcframework.zip",
+			checksum: "f129d9a44f20e8af0f682ac20b1bba5a2e6210fa932cf2df278ce70032beff99"
 		)
 	]
 )
